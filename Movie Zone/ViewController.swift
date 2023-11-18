04cc2f5d3d852f3538731f699568c9913b242a36
Nurlybakyt <begaly606@gmail.com>
2023-11-18 19:12:08 +0600

    Initial Commit

---
+++ Movie Zone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Zone
+//
+//  Created by Dinmukhammed Begaly on 18.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
